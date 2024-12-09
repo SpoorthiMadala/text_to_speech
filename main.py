86241b44fa32e42eafaa79799f2af7398bd398be
SpoorthiMadala <mmadalavenkatesh@gmail.com>
2024-12-09 22:58:23 +0530

    Update main.py

--- main.py
+++ main.py
@@ -12,18 +12,21 @@ os.makedirs(AUDIO_FOLDER, exist_ok=True)
 @app.route("/", methods=["GET", "POST"])
 def index():
     audio_file = None
-    if request.method == "POST":
-        # Get the input text from the form
-        text = request.form.get("text")
-        
-        if text:
-            audio_path = os.path.join(AUDIO_FOLDER, "output_audio.wav")
-            tts = gTTS(text)
-            tts.save(audio_path)
 
-            audio_file = f"/static/audio/output_audio.wav"
+    if request.method == "POST":
+        try:
+            text = request.form.get("text")
+            if text:
+                audio_path = os.path.join(AUDIO_FOLDER, "output_audio.wav")
+                tts = gTTS(text)
+                tts.save(audio_path)
+
+                audio_file = f"/static/audio/output_audio.wav"
+        except Exception as e:
+            return f"An error occurred: {str(e)}"
 
     return render_template("index.html", audio_file=audio_file)
 
 if __name__ == "__main__":
     app.run(debug=True)
+
